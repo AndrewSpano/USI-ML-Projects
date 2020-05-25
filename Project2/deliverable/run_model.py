c54af7cee9e3e41a9a68b442296289bcfe44122f
usi <usi@usis-MacBook-Pro.local>
2020-05-25 02:41:46 +0200

    fixed directory path v2

---
+++ Project2/deliverable/run_model.py
@@ -0,0 +1,107 @@
+# For the models
+from tensorflow.keras.models import load_model
+from tensorflow.keras import utils
+
+# For some calculation at the end
+import numpy as np
+
+# To uses the read function
+import sys
+sys.path.insert(1, '../src')
+
+# Get the load_data() function
+from utils import load_cifar10
+
+
+
+if __name__ == '__main__':
+
+    # Load the test CIFAR-10 data
+    (x_train, y_train), (x_test, y_test) = load_cifar10()
+    # ...
+
+
+    # Preprocessing
+    # Normalize to 0-1 range
+    x_train = x_train / 255.
+    x_test = x_test / 255.
+    # Pre-process targets
+    n_classes = 3
+    y_train = utils.to_categorical(y_train, n_classes)
+    y_test = utils.to_categorical(y_test, n_classes)
+
+
+    # Load the trained models
+    model_task1 = load_model('./nn_task1.h5')
+    model_task2 = load_model('./nn_task2.h5')
+
+
+
+
+    # Print information about the two models that we are going to compare
+    print("\n\nThe summary of the model from Task 1 is:")
+    model_task1.summary()
+    print("\n\nThe summary of the best model found from the grid search (Task 2) is:")
+    model_task2.summary()
+
+
+    # ---------------------------------------------------------- START COMPARISON ----------------------------------------------------------
+
+    # Explain the procedure (this part has been copy-pasted from ../src/file2.py)
+    print("\n\n")
+    print("Let e_1, e_2 denote denote the Classification Accuracies in the test set of the models from Task 1 and Task 2 respectively")
+    print("First we must see if the Null Hypothesis holds: H_0: E[e_1] = E[e_2]")
+    print("We reject H_0 if T (formula in the slides \"4_Model_performance\" page 21, on iCorsi) is outside the 95% confidence interval: (-1.96, 1.96)")
+    print("If it doesn't hold, then the model with the smaller variable is preferable\n")
+
+
+    # Model from Task 1
+
+    # Make the prediction
+    y1_pred = (model_task1.predict(x_test) > .5).astype(int)
+    # Calculate the accuracy for each instance
+    e_1 = (y_test == y1_pred).astype(int)
+    # Calculate the mean accuracy
+    mean_e_1 = e_1.mean()
+    # Calculate the variance squared
+    s2_1 = mean_e_1 * (1 - mean_e_1)
+
+
+
+    # Model from Task 2
+
+    # Make the prediction
+    y2_pred = (model_task2.predict(x_test) > .5).astype(int)
+    # Calculate the accuracy for each instance
+    e_2 = (y_test == y2_pred).astype(int)
+    # Calculate the mean accuracy
+    mean_e_2 = e_2.mean()
+    # Calculate the variance squared
+    s2_2 = mean_e_2 * (1 - mean_e_2)
+
+
+
+
+    # Test statistics: T = (mean_e_1 - mean_e_2) / sqrt(s2_1/l + s2_2/l), where l is the number of test_points
+    l = len(x_test)
+    T = (mean_e_1 - mean_e_2) / np.sqrt(s2_1 / l + s2_2 / l)
+
+
+
+    # Print statistics and finish
+    print("Task 1 model: Mean: {} -- Variance Squared: {}".format(mean_e_1, s2_1))
+    print("Task 2 model: Mean: {} -- Variance Squared: {}".format(mean_e_2, s2_2))
+    print("\nT = {}\n".format(T))
+
+    # Decide Null Hypothesis
+    if T > -1.96 and T < 1.96:
+        print("Null Hypothesis H_0 accepted because: -1.96 < T = {} < 1.96".format(T))
+        print("Therefore, the accuracy levels can be considered as stastically similar\n")
+    else:
+        print("Null Hypothesis H_0 rejected because T = {} is not in the 95% confidence interval: (-1.96, 1.96)")
+        print("Therefore, the model with the smallest variance is preferable, which is:\n")
+        if s2_1 < s2_2:
+            print("Model from Task 1")
+        else:
+            print("Model from Task 2")
+        print("And hence, the accuracy levels can be considered as stastically different\n")
---
+++ Project2/src/file1.py
@@ -0,0 +1,96 @@
+# For creating the model
+from tensorflow.keras import utils, Sequential, optimizers
+from tensorflow.keras.models import load_model
+from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, AveragePooling2D, Flatten, Dropout, LeakyReLU
+from tensorflow.keras.callbacks import EarlyStopping
+from tensorflow.keras.regularizers import l2
+
+
+# Get the load_data() and save_keras_model functions
+from utils import load_cifar10, save_keras_model, plot_history
+
+
+
+# Load the test CIFAR-10 data
+(x_train, y_train), (x_test, y_test) = load_cifar10()
+
+# Normalize to 0-1 range
+x_train = x_train / 255.
+x_test = x_test / 255.
+# Pre-process targets
+n_classes = 3
+y_train = utils.to_categorical(y_train, n_classes)
+y_test = utils.to_categorical(y_test, n_classes)
+
+
+# Build model
+model = Sequential()
+
+''' # ------ THIS PART IS WITH NORMAL ReLU --------
+model.add(Conv2D(8, (5, 5), strides = (1, 1), activation = 'relu', input_shape = (32, 32, 3)))
+'''
+# Add the first convolutional layer with 8 filters of size 5x5, stride of 1x1 and LeakeReLU activation (for bonus)
+model.add(Conv2D(8, (5, 5), strides = (1, 1), activation = 'linear', input_shape = (32, 32, 3)))
+# LeakyReLU activation has to be added as an extra layer, according to https://github.com/tensorflow/tensorflow/issues/27142
+model.add(LeakyReLU(alpha = 0.15))
+
+# Add a Max pooling layer of size 2x2
+model.add(MaxPooling2D(pool_size = (2, 2)))
+
+''' # ------ THIS PART IS WITH NORMAL ReLU --------
+model.add(Conv2D(16, (3, 3), strides = (2, 2), activation = 'relu', input_shape = (32, 32, 3)))
+'''
+# Add the second convolutional layer with 16 filters of size 3x3, stride of 2x2 and LeakeReLU activation (for bonus)
+model.add(Conv2D(16, (3, 3), strides = (2, 2), activation = 'linear', input_shape = (32, 32, 3)))
+# LeakyReLU activation has to be added as an extra layer, according to https://github.com/tensorflow/tensorflow/issues/27142
+model.add(LeakyReLU(alpha = 0.15))
+
+# Add an Average pooling layer of size 2x2
+model.add(AveragePooling2D(pool_size = (2, 2)))
+
+# Add a layer to convert the 2D feature maps to flat vectors
+model.add(Flatten())
+
+# Add a dropout layer with dropout probability of 0.3 before each Dense layer (bonus part)
+model.add(Dropout(0.3))
+
+# Add a dense layer with 8 neurons and tanh activation. Also add L2 regularization with factor 0.005 (bonus part)
+model.add(Dense(8, activation = 'tanh', kernel_regularizer = l2(0.005)))
+
+# Add a dropout layer with dropout probability of 0.3 before each Dense layer (bonus part)
+model.add(Dropout(0.3))
+
+# Add a dense output layer with softmax activation. Also add L2 regularization with factor 0.005 (bonus part)
+model.add(Dense(n_classes, activation = 'softmax', kernel_regularizer = l2(0.005)))
+
+
+# Compile the model
+model.compile(optimizer = optimizers.RMSprop(lr = 0.003),
+                loss = 'categorical_crossentropy',                   
+                metrics = ['accuracy'],
+                )
+model.summary()
+
+# Implementation of early stopping
+my_callback = EarlyStopping(monitor = 'val_acc', patience = 10, restore_best_weights = True)
+
+# Train model
+batch_size = 128
+epochs = 500
+history = model.fit(x_train,
+                    y_train,
+                    batch_size = batch_size,
+                    epochs = epochs,
+                    callbacks = [my_callback],
+                    validation_split = 0.2)
+
+
+# Plotting
+plot_history(history)
+
+# Evaluate model
+scores = model.evaluate(x_test, y_test)
+print('Test loss: {} - Accuracy: {}'.format(*scores))
+
+# Save the model
+save_keras_model(model, "../deliverable/nn_task1.h5")
